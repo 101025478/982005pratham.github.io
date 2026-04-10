2ce3f0719f5a29ce629b93ab82e3956dc0814d87
101025478 <pratham.prajapati@dcmail.ca>
2026-04-09 20:33:05 -0400

    part4 added

---
+++ assignment4/part4/main.js
@@ -0,0 +1,78 @@
+// Name: Pratham Prajapati
+// File: main.js
+// Date: 09 April 2026
+// Description: Bouncing balls using JavaScript (MDN)
+
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+const para = document.querySelector("p");
+
+const width = canvas.width = window.innerWidth;
+const height = canvas.height = window.innerHeight;
+
+let balls = [];
+let ballCount = 0;
+
+// Ball constructor
+function Ball(x, y, velX, velY, size, color) {
+  this.x = x;
+  this.y = y;
+  this.velX = velX;
+  this.velY = velY;
+  this.size = size;
+  this.color = color;
+}
+
+Ball.prototype.draw = function () {
+  ctx.beginPath();
+  ctx.fillStyle = this.color;
+  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+  ctx.fill();
+};
+
+Ball.prototype.update = function () {
+  if ((this.x + this.size) >= width || (this.x - this.size) <= 0) {
+    this.velX = -this.velX;
+  }
+
+  if ((this.y + this.size) >= height || (this.y - this.size) <= 0) {
+    this.velY = -this.velY;
+  }
+
+  this.x += this.velX;
+  this.y += this.velY;
+};
+
+// create balls
+while (balls.length < 10) {
+  const size = 10 + Math.random() * 20;
+
+  const ball = new Ball(
+    Math.random() * (width - size * 2) + size,
+    Math.random() * (height - size * 2) + size,
+    (Math.random() - 0.5) * 10,
+    (Math.random() - 0.5) * 10,
+    size,
+    `rgb(${Math.random()*255},${Math.random()*255},${Math.random()*255})`
+  );
+
+  balls.push(ball);
+  ballCount++;
+}
+
+// animation loop
+function loop() {
+  ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
+  ctx.fillRect(0, 0, width, height);
+
+  for (const ball of balls) {
+    ball.draw();
+    ball.update();
+  }
+
+  para.textContent = "Ball count: " + ballCount;
+
+  requestAnimationFrame(loop);
+}
+
+loop();
